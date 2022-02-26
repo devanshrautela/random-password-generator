c897291c10a1414842cef4ddea684ab1c4b3f7ed
Devansh Rautela <maildevanshrautela@gmail.com>
2022-02-26 09:46:50 +0530

    commit

--- RandomPasswordGenerator.py
+++ RandomPasswordGenerator.py
@@ -34,6 +34,7 @@ def Generator():
     password = ''
     for x in range (0,4):
         password = random.choice(string.ascii_uppercase)+random.choice(string.ascii_lowercase)+random.choice(string.digits)+random.choice(string.punctuation)
+    password = ''.join(random.sample(password, len(password)))
     for y in range(pass_len.get()- 4):
         password = password+random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation)
     pass_str.set(password)
